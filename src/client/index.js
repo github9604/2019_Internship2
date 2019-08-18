420ea7e540e26c0c40855dd539eb5083a31e05a6
bit9604 <qscv9604@gmail.com>
2019-08-19 01:26:40 +0900

    no message

--- src/client/index.js
+++ src/client/index.js
@@ -8,32 +8,33 @@ import thunk from 'redux-thunk';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'mdbreact/dist/css/mdb.css';
 
-import { AllDirectory, SampleFeed, Login, Register, MainPage, App, SearchPage, MyFeed, MyDirectory, UserDirectory, GroupDirectory} from '../client/containers';
+import { AllDirectory, SampleFeed, Login, Register, MainPage, App, SearchPage, MyFeed, MyDirectory, UserDirectory, GroupDirectory } from '../client/containers';
 
 const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
-    <Provider store={store}>
-      <Router>
-        <Route path="/" component={App} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/MainPage" component={MainPage} />
-        <Route path="/SearchPage" component={SearchPage} />
-        {/* <Route path="/MyDirectory/:dir_name" component={MyDirectory} /> */}
-        <Route path="/AllDirectory" component={AllDirectory} />
-        <Route path="/UserDirectory/:dir_name" component={UserDirectory} />
-        <Route exact path="/UserDirectory" component={UserDirectory} />
-        <Route path="/MyDirectory/:dir_name" component={MyDirectory} />
-        <Route exact path="/GroupDirectory" component={GroupDirectory} />
-        <Route path="/GroupDirectory/:dir_name" component={GroupDirectory} />
-        {/* <Switch>
+  <Provider store={store}>
+    <Router>
+      <Route exact path="/" component={() => <Redirect to="/MainPage" />} />
+      <Route path="/" component={App} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+      <Route path="/MainPage" component={MainPage} />
+      <Route path="/SearchPage" component={SearchPage} />
+      {/* <Route path="/MyDirectory/:dir_name" component={MyDirectory} /> */}
+      <Route path="/AllDirectory" component={AllDirectory} />
+      <Route path="/UserDirectory/:dir_name" component={UserDirectory} />
+      <Route exact path="/UserDirectory" component={UserDirectory} />
+      <Route path="/MyDirectory/:dir_name" component={MyDirectory} />
+      <Route exact path="/GroupDirectory" component={GroupDirectory} />
+      <Route path="/GroupDirectory/:dir_name" component={GroupDirectory} />
+      {/* <Switch>
         <Route path="/MyFeed/:name" component={MyFeed} /> */}
-        <Route path="/MyFeed" component={MyFeed} />
-        <Route path="/SampleFeed" component={SampleFeed} />
-        {/* </Switch> */}
-      </Router>
-    </Provider>,
+      <Route path="/MyFeed" component={MyFeed} />
+      <Route path="/SampleFeed" component={SampleFeed} />
+      {/* </Switch> */}
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
